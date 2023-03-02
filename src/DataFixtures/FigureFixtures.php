49e37688ca29436e9cb70f33f3117cb4ebb23f13
Ndl <juliengautiernadal@hotmail.com>
2023-03-02 11:16:50 +0100

    Data fixtures avec fakerPhp sur figures

--- src/DataFixtures/FigureFixtures.php
+++ src/DataFixtures/FigureFixtures.php
@@ -6,14 +6,16 @@ use App\Entity\Figure;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\String\Slugger\SluggerInterface;
-
+use Faker;
 class FigureFixtures extends Fixture
 {
   public function __construct(private SluggerInterface $slugger){}
-    public function load(ObjectManager $manager): void
+  /*  public function load(ObjectManager $manager): void
     {
-      $this->createFigure('Test2', 'testestetetete', 'test2', 'img2', 'vid2', $manager);
-      $this->createFigure('Test3', 'testestetetete', 'test3', 'img3', 'vid3', $manager);
+
+        $this->createFigure('Test2', 'testestetetete', 'test2', 'img2', 'vid2', $manager);
+        $this->createFigure('Test3', 'testestetetete', 'test3', 'img3', 'vid3', $manager);
+
       $manager->flush();
     }
 
@@ -30,4 +32,26 @@ class FigureFixtures extends Fixture
 
       return $figure;
     }
+  }*/
+
+
+  public function load(ObjectManager $manager): void
+  {
+      // use the factory to create a Faker\Generator instance
+      $faker = Faker\Factory::create('fr_FR');
+
+      for($i = 1; $i <= 10; $i++){
+          $figure = new Figure();
+          $figure->setName($faker->text(15));
+          $figure->setDescription($faker->text());
+          $figure->setType($faker->text(15));
+          $figure->setImgName($faker->text(15));
+          $figure->setVidName($faker->text(15));
+          $figure->setSlug($this->slugger->slug($figure->getName())->lower());
+
+          $manager->persist($figure);
+      }
+
+      $manager->flush();
   }
+}
