03d19f4a0bbcb4335484bf27467c74fee3ed4b93
Ndl <juliengautiernadal@hotmail.com>
2023-03-02 12:02:18 +0100

    Envoie données vers vue

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
+use App\Repository\FigureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(FigureRepository $figureRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'figures' => $figureRepository->findBy([],['name' => 'asc']),
         ]);
     }
 }
