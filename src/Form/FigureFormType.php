bde0ba1ad6981b524ff0c296166233ca49df6a8e
Ndl17 <juliengautiernadal@hotmail.com>
2023-05-12 16:45:41 +0200

    correctifs suppression ajout bouton

--- src/Form/FigureFormType.php
+++ src/Form/FigureFormType.php
@@ -4,11 +4,10 @@ namespace App\Form;
 
 use App\Entity\Figure;
 use App\Entity\Groupe;
-use App\Entity\Videos;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,81 +19,71 @@ use Symfony\Component\Validator\Constraints\File;
 
 class FigureFormType extends AbstractType
 {
-  public function buildForm(FormBuilderInterface $builder, array $options): void
-  {
-    $builder
-    ->add('name', TextType::class, [
-      'label' => 'Nom de la figure',
-      'attr' => ['class' => 'form-control'],
-      'required' => true,
-    ])
-    ->add('description', TextareaType::class, [
-      'label' => 'Décrivez votre figure',
-      'attr' => ['class' => 'form-control'],
-      'required' => true,
-    ])
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la figure',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Décrivez votre figure',
+                'attr' => ['class' => 'form-control'],
+                'required' => true,
+            ])
 
-    ->add('imagesFiles', FileType::class, [
-      'label' => 'Uploadez une image',
-      'mapped' => false,
-      'multiple' => true,
-      'required' => false,
-      'attr' => ['class' => 'form-control'],
-      'constraints' => [
-        new All([
-          'constraints' => [
-            new File([
-              'maxSize' => '1024k',
-              'mimeTypesMessage' => 'Veuillez uploader un fichier au format jpeg ou png',
-              'mimeTypes' => [
-                'image/jpeg',
-                'image/png',
-              ]
-            ]),
-          ],
-        ]),
-      ]
-    ])
+            ->add('imagesFiles', FileType::class, [
+                'label' => 'Uploadez une image',
+                'mapped' => false,
+                'multiple' => true,
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new All([
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '1024k',
+                                'mimeTypesMessage' => 'Veuillez uploader un fichier au format jpeg ou png',
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/png',
+                                ],
+                            ]),
+                        ],
+                    ]),
+                ],
+            ])
 
-    ->add('groupe', EntityType::class, [
-      'class' => Groupe::class,
-      'choice_label' => 'name',
-      'label' => 'Groupe de figure',
-      'attr' => ['class' => 'form-control'],
-    ])
+            ->add('groupe', EntityType::class, [
+                'class' => Groupe::class,
+                'choice_label' => 'name',
+                'label' => 'Groupe de figure',
+                'attr' => ['class' => 'form-control'],
+            ])
 
+            ->add('videos', CollectionType::class, [
+                'entry_type' => UrlType::class,
+                'label' => 'Vidéos',
+                'required' => false,
+                'mapped' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'attr' => ['class' => 'form-control'],
+                'entry_options' => [
+                    'attr' => ['class' => 'form-control'],
+                ],
+            ])
 
-    ->add('videos', CollectionType::class, [
-      'entry_type' => UrlType::class,
-      'label' => 'Vidéos',
-      'required' => false,
-      'mapped' => false,
-      'allow_add' => true,
-      'allow_delete' => true,
-      'attr' => ['class' => 'form-control'],
-      'entry_options' => [
-        'attr' => ['class' => 'form-control'],
-      ],
-    ])
+            ->add('Envoyer', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary'],
+            ]);
+    }
 
-/*
-    ->add('videos', ::class, [
-      'label' => 'Vidéo',
-      'required' => false,
-      'mapped' => false,
-      'attr' => ['class' => 'form-control'],
-    ])*/
-
-
-    ->add('Envoyer', SubmitType::class, [
-      'attr' => ['class' => 'btn btn-primary'],
-    ]);
-  }
-
-  public function configureOptions(OptionsResolver $resolver): void
-  {
-    $resolver->setDefaults([
-      'data_class' => Figure::class,
-    ]);
-  }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Figure::class,
+        ]);
+    }
 }
